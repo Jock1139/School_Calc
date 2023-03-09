fc54e985936abb4d75dabf5ad9a6fb06f7495477
Jock1139 <60668916+Jock1139@users.noreply.github.com>
2023-03-09 14:51:28 +0100

    Add files via upload

---
+++ Pythagoras calculator.py
@@ -0,0 +1,12 @@
+from math import sqrt
+#Imports the math module sqrt
+print("Input lengths of shorter triangle sides:")
+a = float(input("a: "))
+b = float(input("a: "))
+#Our input which can be decimals and so on
+
+c = sqrt(a**2 + b**2)
+round(c,2)
+#Rounds c to 2 decimals
+print("The length of the hypotenuse is", round(c,2))
+
