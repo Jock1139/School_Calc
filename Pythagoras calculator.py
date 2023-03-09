34bd16dc46ff167c6346edac940b7a6708c592ea
Jock1139 <60668916+Jock1139@users.noreply.github.com>
2023-03-09 17:48:03 +0100

    Update Pythagoras calculator.py

--- Pythagoras calculator.py
+++ Pythagoras calculator.py
@@ -2,7 +2,7 @@ from math import sqrt
 #Imports the math module sqrt
 print("Input lengths of shorter triangle sides:")
 a = float(input("a: "))
-b = float(input("a: "))
+b = float(input("b: "))
 #Our input which can be decimals and so on
 
 c = sqrt(a**2 + b**2)
